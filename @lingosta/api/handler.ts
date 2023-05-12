0db0125382d6f0c33ecb01f445705565cb8f3ee1
Justin Lee <hello@leejustin.com>
2023-05-12 16:19:15 -0700

    Set up Vercel boilerplate for serverless NodeJS API

---
+++ @lingosta/api/handler.ts
@@ -0,0 +1,11 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default function handler(
+  request: VercelRequest,
+  response: VercelResponse,
+) {
+  response.status(200).json({
+    body: "hi",
+    query: "there",
+  });
+}
