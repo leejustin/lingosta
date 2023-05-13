bd1195366e35ac7f9813650f6e2edf8d79501e9f
Justin Lee <hello@leejustin.com>
2023-05-12 18:20:36 -0700

    Fix build process for common types; set up structure for translations

--- @lingosta/common/dist/types.js
+++ @lingosta/common/dist/types.js
@@ -1,8 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.TestEnum = void 0;
+exports.Language = exports.TestEnum = void 0;
 var TestEnum;
 (function (TestEnum) {
     TestEnum["A"] = "a";
     TestEnum["B"] = "b";
 })(TestEnum = exports.TestEnum || (exports.TestEnum = {}));
+var Language;
+(function (Language) {
+    Language["SPANISH"] = "es";
+    Language["BRAZILIAN_PORTUGUESE"] = "pt-BR";
+    Language["ENGLISH"] = "en";
+    Language["KOREAN"] = "ko";
+})(Language = exports.Language || (exports.Language = {}));
--- @lingosta/common/src/types.ts
+++ @lingosta/common/src/types.ts
@@ -2,3 +2,21 @@ export enum TestEnum {
   A = 'a',
   B = 'b',
 }
+
+export enum Language {
+  SPANISH = "es",
+  BRAZILIAN_PORTUGUESE = "pt-BR",
+  ENGLISH = "en",
+  KOREAN = "ko",
+}
+
+export interface Term {
+source: string;
+target: string;
+weight: number; // range 0.0 - 1.0
+}
+export interface Translation {
+  type: Language;
+  sentence: string;
+  terms: Term[];
+}
