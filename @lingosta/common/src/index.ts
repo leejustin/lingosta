58129c3167a1b47564defdd361b970999fa346b7
Justin Lee <hello@leejustin.com>
2023-05-23 11:30:04 -0700

    Fix merge conflicts

--- @lingosta/common/src/index.ts
+++ @lingosta/common/src/index.ts
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-export { Language } from "./types";
-export type { RawTerm, RawTranslation, Term, Translation, TranslationRequest } from "./types";
-=======
 export {UserGroup, Language, Term, UserTranslation} from "./types";
 export type {TranslationRequest, TranslationResponse, RawTranslationResponse, RawTermResponse} from "./types.api";
 export type {DBTranslation, DBTranslationFields, DBGroup, DBGroupFields} from "./types.db";
 
 export * from "./utils.api";
 export * from "./utils.db";
->>>>>>> 4215f1f2c59075f2682ab6e4751b091a38487d79
 export * from "./utils";
--- @lingosta/common/src/utils.ts
+++ @lingosta/common/src/utils.ts
@@ -1,41 +1,6 @@
-<<<<<<< HEAD
-import {Language, RawTranslation, Translation} from "./types";
-
-// Removes punctuation from a word. This may be revisited to make it language-specific.
-const removePunctuation = (sentence: string): string => {
-  if (!sentence) {
-    return "";
-  }
-
-  const matches = sentence.match(/[^_\W]+/g);
-  if (!matches) {
-    return ""
-  } else {
-    return matches.join(' ');
-  }
-}
-
-export const mapRawTranslation = (sentence: string, rawTranslation: RawTranslation, language: Language): Translation => {
-  const terms = rawTranslation.t.map((rawTerm) => ({
-    source: removePunctuation(rawTerm.a),
-    target: removePunctuation(rawTerm.b),
-    weight: rawTerm.weight / 10,
-  })).filter(
-    (term) => term.source !== "" && term.target !== ""
-  );
-
-  return {
-    type: language,
-    sentence: sentence,
-    terms: terms,
-  };
-}
-
-=======
 import {Language} from "./types";
 
 // This is used to map the Language enum to a human-readable format.
->>>>>>> 4215f1f2c59075f2682ab6e4751b091a38487d79
 export const getLanguageName = (lang: Language): string => {
   switch (lang) {
     case Language.SPANISH:
@@ -46,12 +11,6 @@ export const getLanguageName = (lang: Language): string => {
       return "English";
     case Language.KOREAN:
       return "Korean";
-<<<<<<< HEAD
-    default:
-      return "Unknown";
-  }
-};
-=======
     case Language.FRENCH:
       return "French";
     case Language.GERMAN:
@@ -70,4 +29,3 @@ export const getLanguageName = (lang: Language): string => {
       return "Unknown";
   }
 };
->>>>>>> 4215f1f2c59075f2682ab6e4751b091a38487d79
