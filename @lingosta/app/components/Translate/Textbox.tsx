94d00537e2d92e2c5efde129a2fb5104172cbc47
aznamle <namle1998@hotmail.com>
2023-05-22 17:35:38 -0700

    testbox

--- @lingosta/app/components/Translate/Textbox.tsx
+++ @lingosta/app/components/Translate/Textbox.tsx
@@ -1,29 +1,28 @@
 import React from 'react'
-import { BsTranslate } from 'react-icons/bs';
 import LangListbox from './LangListbox';
 
 const Textbox = () => {
     return (
         <div className=''>
-        <div className='flex space-x-2'>
-        <input
-            className="
-                block
-                drop-shadow-xl
-                p-2.5
-                w-full
-                text-md
-                text-gray-50
-                bg-gray-800
-                rounded-lg
-                border
-                border-gray-300
-            " 
-            placeholder="Translate your sentence here..."
-        >
-        </input>
-        <LangListbox />
-        </div>
+            <div className='flex space-x-2'>
+            <input
+                className="
+                    block
+                    drop-shadow-xl
+                    p-2.5
+                    w-full
+                    text-md
+                    text-gray-50
+                    bg-gray-800
+                    rounded-lg
+                    border
+                    border-gray-300
+                " 
+                placeholder="Translate your sentence here..."
+            >
+            </input>
+            <LangListbox />
+            </div>
         </div>
     )
 }
