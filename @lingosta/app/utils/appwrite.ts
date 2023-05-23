c88b1027432a26952941446e06e8656b358dc242
aznamle <namle1998@hotmail.com>
2023-05-22 20:29:07 -0700

    Implement and map the API request and response to approrpiate types

--- @lingosta/app/utils/appwrite.ts
+++ @lingosta/app/utils/appwrite.ts
@@ -1,4 +1,4 @@
-import { Client, Account } from 'appwrite';
+import { Client, Databases, Account } from 'appwrite';
 
 if (!process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID) {
     throw new Error('Must include Appwrite project id');
