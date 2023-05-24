a2c22b3ddd16054e610c6bbd8a8453ac90c2a1ee
Justin Lee <hello@leejustin.com>
2023-05-23 21:01:08 -0700

    WIP checkpoint prior to merge

--- @lingosta/app/components/GroupSelection.tsx
+++ @lingosta/app/components/GroupSelection.tsx
@@ -37,7 +37,7 @@ const GroupSelection = () => {
                     } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
                   >
                     <ES title="Spanish" className="mr-2 h-5 w-5" aria-hidden="true"/>
-                    Edit
+                    Week 1
                   </button>
                 )}
               </Menu.Item>
---
+++ @lingosta/app/providers/GroupProvider.tsx
@@ -0,0 +1,6 @@
+"use client";
+
+import { useState, useEffect, useContext, createContext } from 'react';
+import { AppwriteException } from 'appwrite';
+import { useRouter } from 'next/navigation';
+
