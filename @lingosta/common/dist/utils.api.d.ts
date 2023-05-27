f40bbf7b43633de57e8c8da52d54af7f7f5bb183
aznamle <namle1998@hotmail.com>
2023-05-27 15:28:47 -0700

    git fix

--- @lingosta/common/dist/utils.api.d.ts
+++
@@ -1,3 +0,0 @@
-import { Language } from "./types";
-import { RawTranslationResponse, TranslationResponse } from "./types.api";
-export declare const mapRawTranslation: (sentence: string, rawTranslation: RawTranslationResponse, language: Language) => TranslationResponse;
