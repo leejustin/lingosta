5f39455c80f94c161ba52ec6201f59372c02c214
aznamle <namle1998@hotmail.com>
2023-05-27 15:56:03 -0700

    removed dist

--- @lingosta/common/dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export { UserGroup, Language, Term, UserTranslation } from "./types";
-export type { TranslationRequest, TranslationResponse, RawTranslationResponse, RawTermResponse } from "./types.api";
-export type { DBTranslation, DBTranslationFields, DBGroup, DBGroupFields } from "./types.db";
-export * from "./utils.api";
-export * from "./utils.db";
-export * from "./utils";
--- @lingosta/common/dist/utils.d.ts
+++
@@ -1,3 +0,0 @@
-import { Language } from "./types";
-export declare const getLanguageName: (lang: Language) => string;
-export declare const getLanguageEmoji: (lang: Language) => string;
