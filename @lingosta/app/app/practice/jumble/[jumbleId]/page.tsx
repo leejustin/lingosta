9ce8dcb109482833897205899669d5e773d1360b
Justin Lee <hello@leejustin.com>
2023-06-07 23:07:44 -0700

    Set up page for displaying Jumbles

--- @lingosta/app/app/practice/jumble/[jumbleId]/page.tsx
+++ @lingosta/app/app/practice/jumble/[jumbleId]/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import React, {useEffect, useState} from "react";
-import {DragDropContext, Draggable, Droppable} from "react-beautiful-dnd";
+import {DragDropContext, Draggable, Droppable, DropResult} from "react-beautiful-dnd";
 import Word from "../../../../components/Jumble/Word";
 import Option from "../../../../components/Jumble/Option";
-import services from "../../../../utils/JumbleUtils";
 import {UserJumble} from "../../../../../common/src/types";
+import {v4 as uuidv4} from 'uuid';
 
 const testJumble: UserJumble = {
   ownerId: "12345",
@@ -39,60 +39,96 @@ const testJumble: UserJumble = {
   updatedAt: new Date(),
 }
 
-interface Item {
+interface JumbleItem {
   id: string;
-  content: string;
+  displayText: string;
 }
 
-interface Rows {
-  [key: string]: {
-    items: Item[];
-  };
-}
+const onDragEnd = (
+  dropResult: DropResult,
+  jumbleOptions: JumbleItem[],
+  userAnswer: JumbleItem[],
+  setUserAnswer: React.Dispatch<React.SetStateAction<JumbleItem[]>>,
+  setJumbleOptions: React.Dispatch<React.SetStateAction<JumbleItem[]>>,
+) => {
+  if (!dropResult.destination) return;
+
+  const { source, destination } = dropResult;
+
+  if (source.droppableId !== destination.droppableId) {
+    // Handle moving an option between question/answer
+    const sourceList = source.droppableId === "Answer" ? userAnswer : jumbleOptions;
+    const destinationList = source.droppableId === "Answer" ? jumbleOptions : userAnswer;
+
+    // Remove the item from the source
+    const movedItem = sourceList[source.index];
+    const modifiedSource = sourceList.filter((j: JumbleItem, index: number) => index !== source.index);
+
+    // Add the item into the destination
+    const modifiedDestination = [...destinationList];
+    modifiedDestination.splice(destination.index, 0, movedItem);
+
+    if (source.droppableId === "Answer") {
+      setJumbleOptions(modifiedDestination);
+      setUserAnswer(modifiedSource);
+    } else {
+      setJumbleOptions(modifiedSource);
+      setUserAnswer(modifiedDestination);
+    }
+  } else {
+    // Handle moving an option within the question/answer
+    const modified = source.droppableId === "Answer" ? [...userAnswer] : [...jumbleOptions];
+    const [item] = modified.splice(source.index, 1);
+    modified.splice(destination.index, 0, item);
+
+    if (source.droppableId === "Answer") {
+      setUserAnswer(modified);
+    } else {
+      setJumbleOptions(modified);
+    }
+  }
+};
 
 const JumblePractice: React.FC = () => {
-  const items: Item[] = [
-    {id: "12345", content: "speak"},
-    {id: "12346", content: "Spanish"},
-    {id: "12347", content: "I"},
-    {id: "12348", content: "MOo"},
-    {id: "123445", content: "speak"},
-    {id: "123d46", content: "Spanish"},
-    {id: "12f347", content: "I"},
-    {id: "12xx348", content: "MOo"},
-    {id: "123f45", content: "speak"},
-    {id: "123dd46", content: "Spanish"},
-    {id: "1234dd7", content: "I"},
-    {id: "12z348", content: "MOo"},
-  ];
-
-  const rowsBackend: Rows = {
-    Question: {
-      items: items,
-    },
-    Answer: {
-      items: [],
-    },
-  };
-
-  const [userAnswer, setUserAnswer] = useState<string[]>([]);
-
-  const [rows, setRows] = useState<Rows | undefined>(rowsBackend);
   const [isWindowReady, setIsWindowReady] = useState(false);
+  const [userAnswer, setUserAnswer] = useState<JumbleItem[]>([]);
+  const [jumbleOptions, setJumbleOptions] = useState<JumbleItem[]>([]);
+  const [answer, setAnswer] = useState<string[]>([]);
 
   useEffect(() => {
+    setJumbleOptions(
+      testJumble.options
+        .sort(() => 0.5 - Math.random())
+        .map((option) => ({
+          id: uuidv4(),
+          displayText: option,
+        }))
+    );
+    setAnswer(testJumble.terms.map((term) => term.target));
     setIsWindowReady(true);
   }, []);
+
+  const handleSubmit = () => {
+    if (
+      userAnswer.map((v) => v.displayText).join(" ") === answer.join(" ")
+    ) {
+      console.log("YAY!");
+    }
+  };
 
   return (
     <div className="mx-auto p-5">
       <div className="flex mx-auto w-full mt-8 text-center justify-center space-y-6 items-center">
-        {/* @ts-ignore */}
-        <DragDropContext onDragEnd={(result) => rows && services.onDragEnd(result, rows, setRows)}>
+        <DragDropContext
+          onDragEnd={(dropResult: DropResult) =>
+            testJumble &&
+            onDragEnd(dropResult, jumbleOptions, userAnswer, setUserAnswer, setJumbleOptions)
+          }
+        >
           <div className="bg-gray-100 md:w-3/5 w-full h-4/5 rounded-xl p-10 shadow-xl">
             <div className="mt-16 flex gap-3 text-lg">
-              {testJumble.terms.map((term) => (
-                <Word text={term.source} hint={term.target}/>
+              {testJumble.terms.map((term, index) => (
+                <Word key={index} text={term.source} hint={term.target} />
               ))}
             </div>
             <div className="mt-10">
@@ -104,15 +140,17 @@ const JumblePractice: React.FC = () => {
                       ref={provided.innerRef}
                       {...provided.droppableProps}
                     >
-
-                      {rows["Answer"].items.map((item, index) => (
+                      {userAnswer.map((item, index) => (
                         <Draggable
                           key={item.id}
                           draggableId={item.id}
                           index={index}
                         >
                           {(provided, snapshot) => (
-                            <Option displayText="hi" provided={provided}/>
+                            <Option
+                              displayText={item.displayText}
+                              provided={provided}
+                            />
                           )}
                         </Draggable>
                       ))}
@@ -121,9 +159,9 @@ const JumblePractice: React.FC = () => {
                   )}
                 </Droppable>
               )}
-              <hr className="border-gray-300"/>
-              <br/>
-              <hr className="mt-5 border-gray-300"/>
+              <hr className="border-gray-300" />
+              <br />
+              <hr className="mt-5 border-gray-300" />
             </div>
             {isWindowReady && (
               <Droppable droppableId="Question" direction="horizontal">
@@ -133,14 +171,17 @@ const JumblePractice: React.FC = () => {
                     ref={provided.innerRef}
                     {...provided.droppableProps}
                   >
-                    {rows["Question"].items.map((item, index) => (
+                    {jumbleOptions.map((item, index) => (
                       <Draggable
                         key={item.id}
                         draggableId={item.id}
                         index={index}
                       >
                         {(provided, snapshot) => (
-                          <Option displayText={"LOL"} provided={provided}/>
+                          <Option
+                            displayText={item.displayText}
+                            provided={provided}
+                          />
                         )}
                       </Draggable>
                     ))}
@@ -150,9 +191,9 @@ const JumblePractice: React.FC = () => {
               </Droppable>
             )}
             <div className="flex justify-center mt-10 md:mt-5">
-              {/* @ts-ignore */}
-              <button onClick={() => services.handleSubmit(rows, setRows, items)}
-                      className="bg-[#2A9D8F] hover:bg-[#264653] transition-all text-white font-medium text-lg px-10 py-2  rounded-lg shadow-lg "
+              <button
+                onClick={() => handleSubmit()}
+                className="bg-[#2A9D8F] hover:bg-[#264653] transition-all text-white font-medium text-lg px-10 py-2 rounded-lg shadow-lg"
               >
                 Check
               </button>
