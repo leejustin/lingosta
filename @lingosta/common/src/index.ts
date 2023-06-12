42e7a2b1cb023b51632ac59155aec5a9437a81ff
Justin Lee <hello@leejustin.com>
2023-06-12 14:58:04 -0700

    Remove modules that have been consolidated into the main project

--- @lingosta/common/src/index.ts
+++
@@ -1,7 +0,0 @@
-export {UserGroup, Language, Term, UserSession, SessionStatus, UserTranslation, UserJumble, UserJumbleSubmission} from "./types";
-export type {TranslationRequest, TranslationResponse, RawTranslationResponse, RawTermResponse} from "./types.api";
-export type {DBSession, DBSessionFields, DBTranslation, DBTranslationFields, DBGroup, DBGroupFields, DBJumble, DBJumbleFields, DBJumbleSubmission, DBJumbleSubmissionFields} from "./types.db";
-
-export * from "./utils.api";
-export * from "./utils.db";
-export * from "./utils";
